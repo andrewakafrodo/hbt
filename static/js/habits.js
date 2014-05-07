4a56caa795a7f205bd4cc00b9fee51aa021f7852
frodosamoa <andrewrkowalczyk@gmail.com>
2014-05-06 17:25:30 -0700

    updated habits.js file
    smooth scrolling to anchors on welcome page, using ajax to update each
    habit individually

--- static/js/habits.js
+++ static/js/habits.js
@@ -1,18 +1,46 @@
 $(function() {
 
-	$('#habit-name-row-table > tbody > tr, #habit-actions').affix({
-	    offset: { top: $('#habit-history').offset().top - $('.navbar').height()}
+	$('.habit-day').on('click', function() {
+		$(this).toggleClass("bg-danger bg-success");
+        $(this).html("<i class=\"fa fa-spinner fa-spin\"></i>");
+
+        // Make the create buttons be unactive.
+        $(this).attr('disabled', 'disabled');
+        var habitClicked = $(this).attr('id');
+
+        var habit = {}
+        habit['_id'] = $(this).attr('id').match(/[a-zA-Z]+([-][a-zA-Z]+)+/)[0].replace(/-/g, ' ');
+        habit['completedIntervals.' + $(this).attr('id').match(/\d+/)[0]] = $(this).hasClass("bg-success");
+
+        $.ajax({
+            type: 'PUT',
+            url: 'http://localhost:8080/habits/put/' + $(this).attr('id').match(/[a-zA-Z]+([-][a-zA-Z]+)+/)[0].replace(/-/g, '%20'),
+            data: JSON.stringify(habit),
+            dataType: 'json',
+            success: function (data, textStatus, jqXHR) {
+                $('#' + habitClicked).attr('disabled', '');
+                $('#' + habitClicked).html($('#' + habitClicked).hasClass("bg-success") ? "<i class=\"fa fa-check-circle-o\"></i>"
+                                                            : "<i class=\"fa fa-circle-o\"></i>");
+            }
+        });
+
 	});
-	
-	$('#habit-table td').on('click', function() {
-		if ($(this).attr('disabled') !== "disabled") {
-			$(this).toggleClass('success danger');
-			$(this).html($(this).hasClass("success") ? "<span class=\"glyphicon glyphicon-ok complete\"></span>"
-													 : "<span class=\"glyphicon glyphicon-remove todo\"></span>");
-			var habitInstance = $(this).attr('name');
-			var value = $("#" + habitInstance).attr('value');
-			$("#" + habitInstance).attr('value', value === "true" ? "false" : "true");
-		}
+
+	$('.habit-row').on('click', function() {
+		$(this).find(".habit-check").toggleClass("glyphicon-ok glyphicon-remove");
+		$(this).toggleClass("checked")
+	});
+
+
+	$(function () { $("[data-toggle='tooltip']").tooltip(); });
+	$(function () { $("[data-toggle='popover']").tooltip(); });
+
+	// Animate the welcome screen.
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $.attr(this, 'href') ).offset().top
+		}, 500);
+		return false;
 	});
 
 });
