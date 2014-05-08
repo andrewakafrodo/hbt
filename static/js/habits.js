0ee92ac81af441128faf14c1a837dc7022c0aad0
frodosamoa <andrewrkowalczyk@gmail.com>
2014-05-07 21:41:12 -0700

    updated js file for ajax calls

--- static/js/habits.js
+++ static/js/habits.js
@@ -26,10 +26,41 @@ $(function() {
 
 	});
 
+    // Check a habit off or on smaller devices.
 	$('.habit-row').on('click', function() {
-		$(this).find(".habit-check").toggleClass("glyphicon-ok glyphicon-remove");
-		$(this).toggleClass("checked")
+		$(this).find(".habit-check").toggleClass("fa-circle-o fa-check-circle-o");
+        $(this).toggleClass("bg-danger bg-success");
+
+        // $.ajax({
+        //     type: 'PUT',
+        //     url: 'http://localhost:8080/habits/put/' + $(this).attr('id').match(/[a-zA-Z]+([-][a-zA-Z]+)+/)[0].replace(/-/g, '%20'),
+        //     data: JSON.stringify(habit),
+        //     dataType: 'json',
+        //     success: function (data, textStatus, jqXHR) {
+        //         $('#' + habitClicked).attr('disabled', '');
+        //         $('#' + habitClicked).html($('#' + habitClicked).hasClass("bg-success") ? "<i class=\"fa fa-check-circle-o\"></i>"
+        //                                                     : "<i class=\"fa fa-circle-o\"></i>");
+        //     }
+        // });
 	});
+
+    // Delete a habit.
+    $('.habit-row').on('click', function() {
+        $(this).find(".habit-check").toggleClass("fa-circle-o fa-check-circle-o");
+        $(this).toggleClass("bg-danger bg-success");
+
+        // $.ajax({
+        //     type: 'PUT',
+        //     url: 'http://localhost:8080/habits/put/' + $(this).attr('id').match(/[a-zA-Z]+([-][a-zA-Z]+)+/)[0].replace(/-/g, '%20'),
+        //     data: JSON.stringify(habit),
+        //     dataType: 'json',
+        //     success: function (data, textStatus, jqXHR) {
+        //         $('#' + habitClicked).attr('disabled', '');
+        //         $('#' + habitClicked).html($('#' + habitClicked).hasClass("bg-success") ? "<i class=\"fa fa-check-circle-o\"></i>"
+        //                                                     : "<i class=\"fa fa-circle-o\"></i>");
+        //     }
+        // });
+    });
 
 
 	$(function () { $("[data-toggle='tooltip']").tooltip(); });
